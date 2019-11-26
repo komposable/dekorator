4d6267e7443a5babb3851ac8e4c224076a9808ab
Nicolas Brousse <n.brousse@pantographe.studio>
2019-11-26 20:57:09 +0100

    Add access to decorate helper in ActionController

--- lib/dekorator/decorators_helper.rb
+++ lib/dekorator/decorators_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 module DecoratorsHelper
-  delegate :decorate, to: "Dekorator::Base"
+  def decorate(object_or_collection, with: nil)
+    Dekorator::Base.decorate(object_or_collection, with: with)
+  end
 end

205a5cf79631bbe914a18d0891e5837a7d86487c
Nicolas Brousse <nicolas-brousse@users.noreply.github.com>
2020-03-05 23:38:36 +0100

    Remove unnecessary eager load configuration

--- lib/dekorator/railtie.rb
+++ lib/dekorator/railtie.rb
@@ -7,9 +7,5 @@ module Dekorator
     config.to_prepare do |_app|
       ActionController::Base.include Dekorator::Controller
     end
-
-    config.after_initialize do |app|
-      app.config.paths.add "app/decorators", eager_load: true
-    end
   end
 end
